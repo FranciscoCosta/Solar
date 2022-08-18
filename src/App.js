1b5186271b36a6f28f5b1783e92c1a330e8b9d1b
Francisco Costa <francisco100eg@gmail.com>
2022-08-18 15:05:06 -0300

    Requisito4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 //Francisco Costa
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (<div>
+    <Header/>
+    <SolarSystem/>
+    </div>
+    );
   }
 }
 
